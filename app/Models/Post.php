c1247b54eff92d65516336f6006866bffc015cad
Kazuki95-hub <km4524771@gmail.com>
2024-03-20 11:20:00 +0900

    共有ボタンの追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,16 +21,16 @@ class Post extends Model
     {
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
-    public function getPaginateByLimit(int $limit_count = 5)
+    public function getPaginateByLimit(int $limit_count = 6)
     {
     // updated_atで降順に並べたあと、limitで件数制限をかける
-        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate(6);
     }
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
-    public function searchPaginateByLimit(string $keyword,int $limit_count = 5 )
+    public function searchPaginateByLimit(string $keyword,int $limit_count =6 )
     {
         return $this->where('title', 'LIKE' ,"%{$keyword}")
                     ->orWhere('body' ,'LIKE',"%{$keyword}")
