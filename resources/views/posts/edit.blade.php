8a6c9af26b65dc28323f0f7fe459f84afe8836b7
Kazuki95-hub <km4524771@gmail.com>
2024-04-02 15:49:17 +0900

    編集画修正

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,24 +6,26 @@
         </head>
         
         <body>
-            <h1>Blog Name</h1>
-            <form action="/posts/{{ $post->id }}" method="POST">
-                @csrf
-                @method('PUT')
-                <div class="title">
-                    <h2>Title</h2>
-                    <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
-                    <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            <div style="margin-left:5px">
+                <h1>Blog Name</h1>
+                <form action="/posts/{{ $post->id }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="title">
+                        <h2>Title</h2>
+                        <input style="width:300px;" type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
+                        <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+                    </div>
+                    <div class="body">
+                        <h2>Body</h2>
+                        <textarea style="width:90%; height:500px;" name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
+                        <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+                    </div>
+                    <input type="submit" value="更新"/>
+                </form>
+                <div class="footer">
+                    <a href="/posts/{{ $post->id }}">戻る</a>
                 </div>
-                <div class="body">
-                    <h2>Body</h2>
-                    <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
-                    <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
-                </div>
-                <input type="submit" value="更新"/>
-            </form>
-            <div class="footer">
-                <a href="/posts/{{ $post->id }}">戻る</a>
             </div>
         </body>
     </x-app-layout>
